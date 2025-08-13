32e6566f2aef4c5c664ce49d755d6f618b1a4071
SeongChoel <tldtlddl1234@gmail.com>
2025-08-13 22:06:53 +0900

    3,4단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package com
 fun main() {
 
     println("====명언 앱=====")
+    var lastId = 0
 
     while (true) {
         print("명령) ")
@@ -16,8 +17,9 @@ fun main() {
                 val saying = readlnOrNull() ?: ""
                 print ("작가: ")
                 val author = readlnOrNull() ?: ""
+                lastId ++
 
-                println("${saying} - ${author} 명언이 등록되었습니다.")
+                println("${lastId}번 명언이 등록되었습니다.")
             }
         }
     }
