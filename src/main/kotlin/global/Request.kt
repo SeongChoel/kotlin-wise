1e36599678ff074cf007288a8fe990c5e24b374b
SeongChoel <tldtlddl1234@gmail.com>
2025-08-14 09:34:52 +0900

    Request refactoring

--- src/main/kotlin/global/Request.kt
+++ src/main/kotlin/global/Request.kt
@@ -1,30 +1,31 @@
 package com.global
+
 //삭제?id=1
 class Request(
     val input: String
 ) {
-    var actionName: String = ""
-    var paramMap = mutableMapOf<String, String>()
+    var actionName: String
+    var paramMap: Map<String, String>
 
     init {
         val inputBits = input.split("?", limit = 2)
         actionName = inputBits[0].trim()
 
-        if (inputBits.size == 2) {
-            val params = inputBits[1].split("&")
-
-            params.forEach { param ->
-                val paramBits = param.split("=", limit = 2)
-
-                if(paramBits.size ==2) {
-                    paramMap[paramBits[0]] = paramBits[1]
-                }
+        paramMap = if (inputBits.size == 2) {
+            inputBits[1].split("&").associate {
+                val bits = it.split("=", limit = 2)
+                val key = bits[0]
+                val value = if (bits.size > 1) bits[1] else ""
+                key to value
             }
+                .toMap()
+        } else {
+            emptyMap()
         }
 
     }
 
-    fun getParam(key :String) : String? {
+    fun getParam(key: String): String? {
         return paramMap[key]
     }
 
